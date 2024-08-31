c2cc5b4fee68cb401c93267830e929552ff15454
Ram <ramana.nathan106@gmail.com>
2024-08-31 22:56:47 +0530

    funcfy predict

--- predict.py
+++ predict.py
@@ -5,24 +5,30 @@ import joblib
 import traceback
 from utils import data_get_preprocess, load_model, model_predict
 
-# Initialize an empty DataFrame to accumulate results
-all_results = pd.DataFrame()
+def predict(tickers):
 
-tickers = ['ARE&M.NS', 'ANGELONE.NS', 'CDSL.NS', 'DRREDDY.NS', 'HUDCO.NS', 'ITC.NS',
- 'KESORAMIND.NS', 'KSCL.NS', 'MANAPPURAM.NS', 'NATCOPHARM.NS', 'SBICARD.NS',
- 'SPANDANA.NS', 'TATACHEM.NS', 'TATAELXSI.NS', 'TATAMTRDVR.NS', 'TATASTEEL.NS', 'WABAG.NS']
+    # Initialize an empty DataFrame to accumulate results
+    all_results = pd.DataFrame()
 
-features = json.load(open("features.json"))
+    features = json.load(open("features.json"))
 
-for ticker in tickers:
-    try:
-        data = data_get_preprocess(ticker, train=False)
-        results = model_predict(data, features, ticker)
-        all_results = pd.concat([all_results, results], ignore_index=True)
-    except:
-        traceback.print_exc()
-        print(f"Error processing {ticker}. Skipping...")
-        continue
+    for ticker in tickers:
+        try:
+            data = data_get_preprocess(ticker, train=False)
+            results = model_predict(data, features, ticker)
+            all_results = pd.concat([all_results, results], ignore_index=True)
+        except:
+            traceback.print_exc()
+            print(f"Error processing {ticker}. Skipping...")
+            continue
 
-print("All results combined:")
-print(all_results)
+    print("All results combined:")
+    print(all_results)
+
+
+if __name__ == "__main__":
+
+    tickers = ['ARE&M.NS', 'ANGELONE.NS', 'CDSL.NS', 'DRREDDY.NS', 'HUDCO.NS', 'ITC.NS',
+    'KESORAMIND.NS', 'KSCL.NS', 'MANAPPURAM.NS', 'NATCOPHARM.NS', 'SBICARD.NS',
+    'SPANDANA.NS', 'TATACHEM.NS', 'TATAELXSI.NS', 'TATAMTRDVR.NS', 'TATASTEEL.NS', 'WABAG.NS']
+    predict(tickers)
