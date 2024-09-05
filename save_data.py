1c8f9f8d43c5a3673ed85be28e8e7bb28fb25060
Ramanathan <ramana.nathan106@gmail.com>
2024-09-05 21:08:22 +0530

    Create save_data.py

---
+++ save_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from utils import data_get_preprocess
+from tickers import tickers
+
+
+if __name__ == "__main__":
+    import traceback
+    
+    df = pd.DataFrame()
+    for ticker in tickers:
+        try:
+            data = data_get_preprocess(ticker, en_idx=-30)
+            df = pd.concat([df, data])
+        except:
+            traceback.print_exc()
+            print(f"Error processing {ticker}. Skipping...")
+            continue
+    print(len(df))
+    df.to_csv("data.csv")
