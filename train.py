83f2eabd38c383bacfdc0db0057d6f5262f720ae
Ramanathan <ramana.nathan106@gmail.com>
2024-09-06 08:46:59 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,18 +1,20 @@
 import pandas as pd
 import json
+import json
 from utils import data_get_preprocess, gen_process_data, train, train_grid
 
+features = [
+            'Return', 'Volatility', 'SMA_5', 'SMA_10', 'SMA_20', 'Momentum', 'RSI',
+            'BB_upper', 'BB_lower', 'MACD', 'Signal_Line', 'Stochastic_K', 'Stochastic_D',
+            'Lag_1', 'Lag_2', 'Lag_3', 'Volume', 'WT_CROSS_LB',
+            'WT1', 'WT2', 'ATR', 'Williams_%R',
+            'VWAP', 'OBV', 'Parabolic_SAR', 'CCI', 'AD_Line', 'CMO',
+            'Ichimoku_Tenkan', 'Ichimoku_Kijun', 'Ichimoku_SenkouA', 'Ichimoku_SenkouB', 'Ichimoku_Chikou',
+            'Last_Week_Return', 'Last_Week_Volatility', 'Last_Week_Momentum', 'Last_Week_SMA', 'Last_Week_RSI',
+            "Market_Cap", "P_E_Ratio", "book_value", "roce", "roe", "debt_to_equity"
+        ]
+
 def func(tickers, st_idx, en_idx, tuning_pram=None):
-    features = [
-                'Return', 'Volatility', 'SMA_5', 'SMA_10', 'SMA_20', 'Momentum', 'RSI',
-                'BB_upper', 'BB_lower', 'MACD', 'Signal_Line', 'Stochastic_K', 'Stochastic_D',
-                'Lag_1', 'Lag_2', 'Lag_3', 'Volume', 'WT_CROSS_LB',
-                'WT1', 'WT2', 'ATR', 'Williams_%R',
-                'VWAP', 'OBV', 'Parabolic_SAR', 'CCI', 'AD_Line', 'CMO',
-                'Ichimoku_Tenkan', 'Ichimoku_Kijun', 'Ichimoku_SenkouA', 'Ichimoku_SenkouB', 'Ichimoku_Chikou',
-                'Last_Week_Return', 'Last_Week_Volatility', 'Last_Week_Momentum', 'Last_Week_SMA', 'Last_Week_RSI',
-                "Market_Cap", "P_E_Ratio", "book_value", "roce", "roe", "debt_to_equity"
-            ]
     with open("features.json", 'w') as d:
         d.write(json.dumps(features))
 
@@ -36,17 +38,24 @@ def func(tickers, st_idx, en_idx, tuning_pram=None):
     else:
         train(X_train, X_test, y_train, y_test)
 
+def train_from_csv(data):
+    X_train, X_test, y_train, y_test, data = gen_process_data(features, data, pred_col="Next_Week_Return")
+    train(X_train, X_test, y_train, y_test)
+
 
 if __name__ == "__main__":
+    import sys
+    #from tickers import tickers
+    # param_grid = {
+    #     'model__dropout_rate': [0.2, 0.3, 0.4],
+    #     'model__neurons': [64, 128, 256],
+    #     'model__init': ['uniform', 'normal'],
+    #     'model__loss': ['mean_squared_error', 'mean_absolute_error', 'huber_loss'],
+    #     'batch_size': [16, 32, 64],
+    #     'epochs': [50, 100],
+    #     'optimizer': ['adam', 'rmsprop']
+    # }
 
-    from tickers import tickers
-    param_grid = {
-        'model__dropout_rate': [0.2, 0.3, 0.4],
-        'model__neurons': [64, 128, 256],
-        'model__init': ['uniform', 'normal'],
-        'model__loss': ['mean_squared_error', 'mean_absolute_error', 'huber_loss'],
-        'batch_size': [16, 32, 64],
-        'epochs': [50, 100],
-        'optimizer': ['adam', 'rmsprop']
-    }
-    func(tickers, 0, -30, param_grid)
+    pth = sys.argv[1]
+    df = pd.read_csv(pth)
+    train_from_csv(data)
